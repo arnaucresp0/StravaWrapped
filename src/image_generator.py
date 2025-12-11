e856ff3cdaed6a9a0bf9703247d73db0cbfc870a
arnaucresp0 <acs2000f20@gmail.com>
2025-12-12 00:15:17 +0100

    add some ToDo's for tomorrow

--- src/image_generator.py
+++ src/image_generator.py
@@ -15,14 +15,16 @@ TEMPLATES = {
             "total_time_minutes":   {"pos": (200, 1030), "size": 80, "color": "white"},
             "total_distance_km":    {"pos": (200, 1170), "size": 80, "color": "white"},
         }
-    },
+    }, 
+    # Done!
     "liked_activity": {
         "file": "assets/wrapped_cat/input/liked_activity_cat.png",
         "fields": {
-            "activty_name":  {"pos": (200, 620), "size": 80, "color": "white"},
-            "activity_kudos": {"pos": (200, 620), "size": 80, "color": "white"},
+            "activty_name":  {"pos": (540, 1280), "size": 44, "color": "white"},
+            "activity_kudos": {"pos": (540, 1390), "size": 44, "color": "white"},
         }
     },
+    #TODO: DEFINE THE TEXT POSITIONS...
     "random_data_cat": {
         "file": "assets/wrapped_cat/input/random_data_cat.png",
         "fields": {
@@ -33,6 +35,7 @@ TEMPLATES = {
             "train_time":    {"pos": (200, 1170), "size": 80, "color": "white"},
         }
     },
+    #TODO: DEFINE THE TEXT POSITIONS...
     "total_elevation_cat": {
         "file": "assets/wrapped_cat/input/total_elevation_cat.png",
         "fields": {
@@ -40,6 +43,7 @@ TEMPLATES = {
             "everest_count":    {"pos": (200, 740), "size": 80, "color": "white"},
         }
     },
+    #TODO: DEFINE THE TEXT POSITIONS...
     "total_km_cat": {
         "file": "assets/wrapped_cat/input/total_elevation_cat.png",
         "fields": {
@@ -47,14 +51,29 @@ TEMPLATES = {
             "distance_comp":    {"pos": (200, 740), "size": 80, "color": "white"},
         }
     },
+    #TODO: DEFINE THE TEXT POSITIONS...
+    "total_time_cat": {
+        "file": "assets/wrapped_cat/input/total_time_cat.png",
+        "fields": {
+            "total_time_minutes":  {"pos": (200, 620), "size": 80, "color": "white"},
+            "total_time_days":    {"pos": (200, 740), "size": 80, "color": "white"},
+        }
+    },
+    #TODO: DEFINE THE TEXT POSITIONS...
     "total_pr_cat": {
-        "file": "assets/wrapped_cat/input/total_elevation_cat.png",
+        "file": "assets/wrapped_cat/input/total_pr_cat.png",
         "fields": {
             "total_pr":  {"pos": (200, 620), "size": 80, "color": "white"},
         }
     },
-    
-    
+    #TODO: DEFINE THE TEXT POSITIONS...
+    "total_watts_cat": {
+        "file": "assets/wrapped_cat/input/total_watts_cat.png",
+        "fields": {
+            "total_kWh":  {"pos": (200, 620), "size": 80, "color": "white"},
+            "total_house_power":    {"pos": (200, 740), "size": 80, "color": "white"},
+        }
+    }
 }
 
 def render_template(template_key, stats, output_path):
